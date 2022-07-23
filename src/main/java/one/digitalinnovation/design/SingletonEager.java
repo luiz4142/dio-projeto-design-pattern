1745d5839df0f5561efc08bfd422163200c78b21
luiz4142 <luiz4142@gmail.com>
2022-07-23 10:39:25 -0300

    Commit inicial

---
+++ src/main/java/one/digitalinnovation/design/SingletonEager.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.design;
+
+public class SingletonEager {
+    private static final SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstancia(){
+        return instancia;
+    }
+}
